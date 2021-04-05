df4dc0b94777005b4ec6ffd76aa946b5998f204d
sirawit khuntee <benzkub13@gmail.com>
2021-04-05 17:01:57 +0700

    update

--- 1.php
+++ 1.php
@@ -1,1 +1,46 @@
-111111111
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'codephp62';
+
+$myconn = new mysqli ($dbhost, $dbuser, $dbpass, $dbname);                  
+if ($myconn->connect_errno){
+    printf("Connect failed: %s\n",$myconn->connect_error);
+    exit();
+}
+
+$strSQL = "SELECT `id`, `username`, `status` FROM `user`";
+$result = $myconn->query($strSQL);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+        <table border="1" width="100%">  
+        <tr>
+            <td>ลำดับ</td>
+            <td>ชื่อผู้ใช้</td>
+            <td>สถานะ</td>
+        </tr>
+        <?php
+        while($row = $result->fetch_array()) {
+    ?>
+        <tr>
+            <td><?php echo $row["id"];?></td>
+            <td><?php echo $row["username"];?></td>
+            <td><?php echo $row["status"];?></td>
+        </tr>
+       <?php
+        }
+    ?>
+    </table>
+</body>
+</html>
---
+++ update.php
@@ -0,0 +1,66 @@
+<?php
+require_once 'connectdb.php';
+$id = "";
+$username = "";
+$status = "";
+// $strSQL = "";
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $id = "";
+    if (isset($_GET["id"]) && $_GET["id"] != '');
+    $id = $_GET["id"];
+    $username = $_GET["username"];
+    $status = $_GET["status"];
+} else {
+    //echo "id is null";
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $status = "";
+    $id = $_GET["id"];
+    $username = $_POST["username"];
+    $status = $_POST["status"];
+    //echo $username . " _ _ " .$status;
+        $strSQL = "UPDATE user SET username`='".$username."',status`=".$status." WHERE id=" . $id;
+        if (($username == "") && ($status == "")); {
+            //echo "ผิดพลาด";
+
+        $result = $myconn->query($strSQL);
+        if ($result) {
+            echo "success";
+        } else {
+            echo "error";
+        }
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="update.php?id=<?=$id?>" method="POST">
+        <table border="10">
+            <tr>
+                <td>username</td>
+                <td><input type="text" name="username" value="<?= $username ?>"></td>
+            </tr>
+            <tr>
+                <td>status</td>
+                <td><input type="text" name="status" value="<?= $status ?>"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" value="save"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
